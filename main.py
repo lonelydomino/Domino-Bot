8888d170bcbe6259716d51d1f0a5f85b94c1d271
lonelydomino <3646917-lonelydomino@users.noreply.replit.com>
2023-07-19 21:13:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import os
+import openai
+import sys
+
+client = discord.Client(intents=discord.Intents.all())
+
+try:
+  openai.api_key = os.environ['OPENAI_API_KEY']
+except KeyError:
+  sys.stderr.write("""
+  You haven't set up your API key yet.
+  """)
+  exit(1)
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith("Domino-Bot") or msg.startswith("Domino-bot") or msg.startswith("domino-bot"):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": message.content}
+    ]
+)
+    await message.channel.send(response.choices[0].message.content)
+
+client.run(os.getenv("TOKEN"))
